10eb2d015e250f3c4fbe640e0b75ee203354a9a6
Mercurio <47455213+NotLugozzi@users.noreply.github.com>
2024-06-23 15:22:37 +0200

    Remove switcheroo gfx controller

--- build.sh
+++ build.sh
@@ -43,6 +43,5 @@ rpm-ostree override replace --experimental --from repo=updates glibc32 || true &
 rpm-ostree override remove mesa-va-drivers-freeworld
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:bazzite-multilib mesa-filesystem mesa-libxatracker mesa-libglapi mesa-dri-drivers mesa-libgbm mesa-libEGL mesa-vulkan-drivers mesa-libGL pipewire pipewire-alsa pipewire-gstreamer pipewire-jack-audio-connection-kit pipewire-jack-audio-connection-kit-libs pipewire-libs pipewire-pulseaudio pipewire-utils bluez bluez-obexd bluez-cups bluez-libs xorg-x11-server-Xwayland && \
 rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld.x86_64 libaacs libbdplus libbluray 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:sentry:switcheroo-control_discrete switcheroo-control 
 rpm-ostree install @workstation-product-environment
 rpm-ostree install screen blender discord firefox ffmpeg steam vlc
